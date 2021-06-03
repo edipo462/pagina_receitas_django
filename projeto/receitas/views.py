aff2200f90aeb0f0d7936474123627278e56800d
Edipo de Almeida <edipo.informatica@gmail.com>
2021-06-03 19:28:35 -0300

    version1

--- projeto/receitas/views.py
+++ projeto/receitas/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1: 'Sorvete',
+        2: 'Bolo de cenoura',
+        3: 'Torta de limão'
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
